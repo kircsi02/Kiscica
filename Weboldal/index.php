15f66bb51cd92a0ad549a66dfa8cc716716f79b6
Kiskacsa15 <zsuliem2111@gmail.com>
2022-10-11 01:50:52 +0200

    Főoldal hozzáadása
    index.php

---
+++ Weboldal/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles/mainstyle.css"/>
+</head>
+<body>
+    <?php
+    require_once 'login.php';
+    ?>
+</body>
+</html>
